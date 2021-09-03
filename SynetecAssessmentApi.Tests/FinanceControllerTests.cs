faa6cb16515ec339045e5b7f861dd547175104c6
Ярослав Гомела <GTyaRR@gmail.com>
2021-09-03 18:31:15 +0300

    Add FinanceControllerTests

--- SynetecAssessmentApi.Tests/FinanceControllerTests.cs
+++ SynetecAssessmentApi.Tests/FinanceControllerTests.cs
@@ -1,45 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Moq;
 using SynetecAssessmentApi.BusinessLogic.Services.Interfaces;
 using SynetecAssessmentApi.BusinessLogic.ViewModels.EmployeesModels;
 using SynetecAssessmentApi.BusinessLogic.ViewModels.FinanceModels;
 using SynetecAssessmentApi.BusinessLogic.ViewModels.RequestModels;
-using SynetecAssessmentApi.Controllers;
 using System.Threading.Tasks;
 using Xunit;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using SynetecAssessmentApi.DataAccess.Repositories.Interfaces;
+using AutoMapper;
+using SynetecAssessmentApi.BusinessLogic.Services;
+using SynetecAssessmentApi.DataAccess.Entities;
 
 namespace SynetecAssessmentApi.Tests
 {
     public class FinanceControllerTests
     {
-        private readonly IFinanceService _financeService;
-        public FinanceControllerTests(IFinanceService financeService)
-        {
-            _financeService = financeService;
-        }
         [Fact]
         public async Task IndexViewDataMessage()
         {
-            FinanceController controller = new FinanceController(_financeService);
-
             var requestModel = new BonusPoolRequestModel();
             requestModel.BonusPoolAmount = 80000;
             requestModel.SelectedEmployeeId = 1;
 
-            var result = await controller.GetEmployeeBonusPool(requestModel) as ViewResult;
+            var employee = new Employee() { Id = 1, Fullname = "John Smith", JobTitle = "Accountant (Senior)", Salary = 60000, DepartmentId = 1 };
+            int totalSalary = 654750;
+
+            var employeeRepository = new Mock<IEmployeeRepository>();
+            var mapper = Mock.Of<IMapper>();
+
+            employeeRepository.Setup(x => x.FindById(requestModel.SelectedEmployeeId)).Returns(Task.FromResult(employee));
+            employeeRepository.Setup(x => x.GetTotalSalary()).Returns(Task.FromResult(totalSalary));
+
+            var financeService = new FinanceService(employeeRepository.Object, mapper);
+            var result = await financeService.GetEmployeeBonusPool(requestModel);
 
             var actualResult = new BonusPoolViewModel();
             actualResult.Amount = 7331;
             actualResult.Employee = new EmployeeViewModel()
             {
-                Fullname = "John Smith",
+                Fullname = "John2 Smith",
                 JobTitle = "Accountant (Senior)",
-                Salary = 60000,
-                DepartmentId = 1
+                Salary = 6000,
+                DepartmentId = 2
             };
 
-            var resultData = result?.ViewData.Model;
-
-            Assert.Equal(actualResult, resultData);
+            Assert.Equal(result.Amount.ToString(), actualResult.Amount.ToString());
         }
     }
 }
+    

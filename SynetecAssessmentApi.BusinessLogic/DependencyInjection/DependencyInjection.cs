22a43a2bedc086ff059c10e3c39bbd882f1bc865
Ярослав Гомела <GTyaRR@gmail.com>
2021-09-03 13:53:15 +0300

    Add identity injection. Set project dependencies

---
+++ SynetecAssessmentApi.BusinessLogic/DependencyInjection/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SynetecAssessmentApi.DataAccess.Repositories;
+using SynetecAssessmentApi.DataAccess.Repositories.Interfaces;
+
+namespace SynetecAssessmentApi.BusinessLogic.DependencyInjection
+{
+    public static class DependencyInjection
+    {
+        public static void SetDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+        }
+    }
+}
